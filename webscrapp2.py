48fe2d29d867eff1975c14cefc0f9576c096756e
Celine Mwango <celinemwango2@gmail.com>
2019-04-24 18:04:05 -0600

    Add files via upload
    This is a program I created to scrap all the words in the new york times post that had the <p> tag.
    
    I have not completed and therefore I will keep updating it

---
+++ webscrapp2.py
@@ -0,0 +1,22 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+base_url = requests.get("https://www.nytimes.com/2019/04/24/technology/facebook-ftc-fine-privacy.html?action=click&module=Top%20Stories&pgtype=Homepage")
+
+# create a beautiful soup object
+
+soup = BeautifulSoup(base_url.text, "html.parser")
+
+# I am going to pull all the text from the body
+
+headers_list = soup.find(class_="css-v89234")
+
+# pull all the texts from the <p> tag within the div
+headers_list_words = headers_list.find_all("p")
+
+
+
+for words in headers_list_words:
+    paragraphs = words.contents
+    print(paragraphs)
